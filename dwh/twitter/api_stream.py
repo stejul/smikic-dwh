d2bddf3541039067d9488eb533d807644cb1ea07
stejul <stejul@rocketmail.com>
2021-06-07 19:12:00 +0200

    feat(twitter)(WIP): sample twitter stream

--- dwh/twitter/api_stream.py
+++ dwh/twitter/api_stream.py
@@ -1,12 +1,40 @@
 from dotenv import load_dotenv
-from kafka import KafkaConsumer, KafkaProducer
+from kafka import KafkaProducer
+from tweepy import Stream, StreamListener, OAuthHandler
+
+from datetime import datetime
 import os
+import time
 
 load_dotenv()
 
-TWITTER_BEARER = os.getenv("TWITTER_BEARER_TOKEN")
+TWITTER_CONSUMER_KEY= os.getenv("TWITTER_CONSUMER_KEY")
+TWITTER_CONSUMER_KEY_SECRET = os.getenv("TWITTER_CONSUMER_KEY_SECRET")
+TWITTER_ACCESS_TOKEN = os.getenv("TWITTER_ACCESS_TOKEN")
+TWITTER_ACCESS_TOKEN_SECRET = os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
 TWITTER_URL = "https://api.twitter.com/2/tweets/search/stream"
 
 
-producer = KafkaProducer(bootstrap_server="localhost:9092")
-topic_name = "get-twitter-stream"
+class TwitterStreamListener(StreamListener):
+    def on_data(self, data):
+        producer.send("dataScience", data.encode("utf-8"))
+        print(data)
+        return True
+    def on_error(self, status):
+        print(status)
+
+producer = KafkaProducer(bootstrap_servers="localhost:9092")
+listener = TwitterStreamListener()
+
+auth = OAuthHandler(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_KEY_SECRET)
+auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
+
+stream = Stream(auth, listener)
+
+def periodicWork(interval):
+    while True:
+        #stream.filter(track=["Data Science"])
+        stream.sample()
+        time.sleep(interval)
+
+periodicWork(60 * 0.1)
